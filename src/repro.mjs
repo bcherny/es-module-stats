0101512d1984439ce7c72608537a69745483d8f2
Boris Cherny <boris@performancejs.com>
2024-06-19 09:48:42 +0200

    add expected output

--- src/repro.mjs
+++ src/repro.mjs
@@ -4,11 +4,12 @@ async function main() {
   const largestNumbers = readFileSync("data/repro.csv", "utf8")
     .split("\n")
     .map((_) => parseInt(_))
+    .filter((_) => _ > 0)
     .sort((a, b) => b - a)
     .slice(0, 100);
 
   writeFileSync(
-    "./data/v8-sort-bug.json",
+    "./data/v8-sort-bug-fixed.json",
     JSON.stringify(largestNumbers, null, 4)
   );
 }
